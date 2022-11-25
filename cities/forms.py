73d64aa2db259b61b29eee12c03c5595c68ba69d
adminpc <newandy321@gmail.com>
2022-11-25 09:17:46 +0200

    create table on home.html and paginator

---
+++ cities/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from cities.models import City
+
+
+class HtmlForm(forms.Form):
+    name = forms.CharField(label='Місто')
+
+
+class CityForm(forms.ModelForm):
+    name = forms.CharField(label='Місто', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Введить назву міста'}))
+
+    class Meta:
+        model = City
+        fields = ('name',)
